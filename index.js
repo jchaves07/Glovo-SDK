0f0affb01ee2ab2cac03a95a8c77f379799c62b8
jorge chaves ulate <jorge.chaves@me.com>
2020-04-10 02:54:53 -0600

    test CI

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const endpointsHelper = require('./src/endpointsHelper');
-
+//get estimate order price
 module.exports.EstimateOrderPrice = (estimateRequest) =>{
     endpointsHelper.post('/b2b/orders/estimate', estimateRequest);
 }
